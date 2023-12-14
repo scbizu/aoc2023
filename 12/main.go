b80fcd70c80341386d0f98fbd3e4ace136914f54
scnace <scbizu@gmail.com>
2023-12-14 08:54:38 +0800

    day12: p1 and half p2

--- 12/main.go
+++ 12/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/magejiCoder/set"
 	"github.com/scbizu/aoc2022/helper/input"
 )
 
@@ -15,112 +16,85 @@ type sequence struct {
 	seq       []byte
 	formation []int
 	resSeq    []byte
+	resSet    *set.Set[string]
 }
 
 func (s sequence) String() string {
 	return fmt.Sprintf("seq: %s,  formation: %+v", string(s.seq), s.formation)
 }
 
-func (s sequence) count() sequence {
-	fmt.Printf("%s\n", s)
+func (s *sequence) count() {
+	// fmt.Printf("%s\n", s)
 	if len(s.formation) == 0 {
-		total++
-		return s
+		if len(s.seq) > 0 {
+			for i := 0; i < len(s.seq); i++ {
+				if s.seq[i] == '#' {
+					return
+				}
+				s.resSeq = append(s.resSeq, '.')
+			}
+		}
+		if !s.resSet.Has(string(s.resSeq)) {
+			// fmt.Printf("res: %s\n", string(s.resSeq))
+			total++
+		}
+		s.resSet.Add(string(s.resSeq))
+		return
 	}
 	if len(s.seq) == 0 {
-		return s
+		return
 	}
 	n := s.formation[0]
 	switch s.seq[0] {
 	case '.':
-		newSeq := sequence{
-			seq:       s.seq[1:],
-			formation: s.formation,
-		}
-		newSeq.count()
+		s.seq = s.seq[1:]
+		s.resSeq = append(s.resSeq, '.')
+		s.count()
 	case '#':
 		switch {
-		case len(s.seq) < n || strings.Contains(string(s.seq[:n]), "."):
-			newSeq := sequence{
-				seq:       s.seq[1:],
-				formation: s.formation,
-			}
-			newSeq.count()
 		case len(s.seq) == n && !strings.Contains(string(s.seq[:n]), "."):
-			newSeq := sequence{
-				seq:       s.seq[n:],
-				formation: s.formation[1:],
+			for i := 0; i < n; i++ {
+				s.resSeq = append(s.resSeq, '#')
 			}
-			newSeq.count()
+			s.seq = s.seq[n:]
+			s.formation = s.formation[1:]
+			s.count()
 		case len(s.seq) > n && !strings.Contains(string(s.seq[:n]), ".") && s.seq[n] != '#':
-			newSeq := sequence{
-				seq:       s.seq[n+1:],
-				formation: s.formation[1:],
+			s.seq = s.seq[n+1:]
+			s.formation = s.formation[1:]
+			for i := 0; i < n; i++ {
+				s.resSeq = append(s.resSeq, '#')
 			}
-			newSeq.count()
+			s.resSeq = append(s.resSeq, '.')
+			s.count()
+		default:
+			return
 		}
 	case '?':
-		tmpSeq := make([]byte, len(s.seq))
-		copy(tmpSeq, s.seq)
-		tmpSeq[0] = '.'
-		newSeq := sequence{
-			seq:       tmpSeq,
+		s1 := make([]byte, len(s.seq))
+		copy(s1, s.seq)
+		s1[0] = '.'
+		s1q := sequence{
+			seq:       s1,
 			formation: s.formation,
+			resSeq:    s.resSeq,
+			resSet:    s.resSet,
 		}
-		newSeq.count()
-		tmpSeq = make([]byte, len(s.seq))
-		copy(tmpSeq, s.seq)
-		tmpSeq[0] = '#'
-		newSeq = sequence{
-			seq:       tmpSeq,
+		// newSeq.resSeq = append(newSeq.resSeq, '.')
+		s1q.count()
+		// fmt.Printf("? -> #: %s\n", s.seq)
+		s2 := make([]byte, len(s.seq))
+		copy(s2, s.seq)
+		s2[0] = '#'
+		s2q := sequence{
+			seq:       s2,
 			formation: s.formation,
+			resSeq:    s.resSeq,
+			resSet:    s.resSet,
 		}
-		newSeq.count()
+		// newSeq.resSeq = append(newSeq.resSeq, '#')
+		s2q.count()
 	}
-	return s
-	// var i int
-	// for {
-	// 	n := s.formation[0]
-	// 	if n > len(s.seq) {
-	// 		break
-	// 	}
-	// 	if i == n {
-	// 		if len(s.seq) > 0 {
-	// 			s.seq = s.seq[1:]
-	// 		}
-	// 		if len(s.formation) > 0 {
-	// 			s.formation = s.formation[1:]
-	// 		}
-	// 		newSeq := s.count()
-	// 		// fmt.Printf("newSeq: %s\n", newSeq)
-	// 		s.arranges += newSeq.arranges
-	// 		return s
-	// 	}
-	// 	seq := s.seq[0]
-	// 	if seq == '.' {
-	// 		s.seq = s.seq[1:]
-	// 		continue
-	// 	}
-	// 	if seq == '?' && len(s.seq) > sum(s.formation) {
-	// 		dot := sequence{
-	// 			seq:       s.seq[1:],
-	// 			arranges:  s.arranges,
-	// 			formation: s.formation,
-	// 		}
-	// 		s.arranges += dot.count().arranges
-	// 		sharp := sequence{
-	// 			seq:       s.seq[1:],
-	// 			arranges: s.arranges,
-	// 			formation: s.formation[1:],
-	// 		}
-	// 		s.arranges += sharp.count().arranges
-	// 	}
-	// 	if seq == '#'  {
-	// 		i++
-	// 		s.seq = s.seq[1:]
-	// 	}
-	// }
-	return s
 }
 
 func sum(s []int) int {
@@ -135,6 +109,100 @@ func sum(s []int) int {
 }
 
 func main() {
+	p1()
+	p2()
+}
+
+// func p2() {
+// 	txt := input.NewTXTFile("./input.txt")
+// 	var allTotal int
+// 	txt.ReadByLineEx(context.Background(), func(_ int, line string) error {
+// 		if line == "" {
+// 			return nil
+// 		}
+// 		total = 0
+// 		parts := strings.Split(line, " ")
+// 		if len(parts) != 2 {
+// 			panic("invalid input")
+// 		}
+// 		numStrs := strings.Split(parts[1], ",")
+// 		seq := sequence{
+// 			seq:       []byte(parts[0]),
+// 			formation: numStrsToNums(numStrs),
+// 			resSet:    set.New[string](),
+// 		}
+// 		seq.count()
+// 		p1Total := total
+// 		total = 0
+// 		var s []byte
+// 		s = append(s, []byte(parts[0])...)
+// 		s = append(s, '?')
+// 		s = append(s, []byte(parts[0])...)
+// 		seq = sequence{
+// 			seq:       s,
+// 			formation: double(numStrsToNums(numStrs)),
+// 			resSet:    set.New[string](),
+// 		}
+// 		seq.count()
+// 		rate := total / p1Total
+// 		allTotal += total * rate * rate * rate
+// 		fmt.Printf("total: %d,rate: %d, p1Total: %d,allTotal: %d\n", total, rate, p1Total, total*rate*rate*rate)
+// 		return nil
+// 	})
+// 	fmt.Fprintf(os.Stdout, "p2: %d\n", allTotal)
+// }
+
+func p2() {
+	txt := input.NewTXTFile("./input.txt")
+	total = 0
+	txt.ReadByLineEx(context.Background(), func(_ int, line string) error {
+		if line == "" {
+			return nil
+		}
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			panic("invalid input")
+		}
+		numStrs := strings.Split(parts[1], ",")
+		seq := sequence{
+			seq:       unfoldSeq([]byte(parts[0])),
+			formation: unfoldFormation(numStrsToNums(numStrs)),
+			resSet:    set.New[string](),
+		}
+		seq.count()
+		return nil
+	})
+	fmt.Fprintf(os.Stdout, "p2: %d\n", total)
+}
+
+func unfoldSeq(s []byte) []byte {
+	var res []byte
+	for i := 0; i < 5; i++ {
+		res = append(res, s...)
+		if i != 4 {
+			res = append(res, '?')
+		}
+	}
+	return res
+}
+
+func unfoldFormation(s []int) []int {
+	var res []int
+	for i := 0; i < 5; i++ {
+		res = append(res, s...)
+	}
+	return res
+}
+
+// func double[T comparable](s []T) []T {
+// 	var res []T
+// 	for i := 0; i < 2; i++ {
+// 		res = append(res, s...)
+// 	}
+// 	return res
+// }
+
+func p1() {
 	txt := input.NewTXTFile("./input.txt")
 	txt.ReadByLineEx(context.Background(), func(_ int, line string) error {
 		if line == "" {
@@ -148,6 +216,7 @@ func main() {
 		seq := sequence{
 			seq:       []byte(parts[0]),
 			formation: numStrsToNums(numStrs),
+			resSet:    set.New[string](),
 		}
 		seq.count()
 		return nil
