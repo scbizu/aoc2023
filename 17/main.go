b44ea23ef0d77d891241934647e254b816a60aba
scnace <scbizu@gmail.com>
2023-12-20 02:01:49 +0800

    day17:ref fastpath

--- 17/main.go
+++ 17/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	defaultLimit = 3
+	defaultLimit = 2
 )
 
 type direction uint8
@@ -22,29 +22,50 @@ const (
 	right
 )
 
-func (d direction) String() string {
-	switch d {
-	case up:
-		return "👆"
-	case down:
-		return "👇"
-	case left:
-		return "<-"
-	case right:
-		return "->"
-	}
-	panic("invalid direction")
-}
-
 var caches = make(map[matrix.Point[int]]int)
 
 type vec struct {
-	row, col int
-	d        direction
+	matrix.Point[int] // point
+	d                 directionLimit
 }
 
 type heatLossMap struct {
 	m matrix.Matrix[int]
+}
+
+func (h heatLossMap) Print() {
+	for i := 0; i < h.m.Rows(); i++ {
+		for j := 0; j < h.m.Cols(); j++ {
+			fmt.Printf("%d ", caches[matrix.Point[int]{
+				X:     i,
+				Y:     j,
+				Value: h.m.Get(i, j),
+			}])
+		}
+		fmt.Println()
+	}
+}
+
+func (h heatLossMap) neighbor(x, y int) []matrix.Point[int] {
+	var ns []matrix.Point[int]
+	if y < h.m.Cols()-1 {
+		ns = append(ns, matrix.Point[int]{X: x, Y: y + 1, Value: h.m.Get(x, y+1)})
+	}
+	if x < h.m.Rows()-1 {
+		ns = append(ns, matrix.Point[int]{X: x + 1, Y: y, Value: h.m.Get(x+1, y)})
+	}
+	if x > 0 {
+		ns = append(ns, matrix.Point[int]{X: x - 1, Y: y, Value: h.m.Get(x-1, y)})
+	}
+	if y > 0 {
+		ns = append(ns, matrix.Point[int]{X: x, Y: y - 1, Value: h.m.Get(x, y-1)})
+	}
+	return ns
+}
+
+type directionValue struct {
+	d direction
+	v int
 }
 
 func getDirection(p1, p2 matrix.Point[int]) direction {
@@ -66,40 +87,66 @@ func getDirection(p1, p2 matrix.Point[int]) direction {
 type directionLimit map[direction]int
 
 func (h *heatLossMap) fastPath(
-	p matrix.Point[int],
-	value int,
-	dl directionLimit,
+	points []vec,
 ) {
+	if len(points) == 0 {
+		return
+	}
+	p := points[0]
+	dl := p.d
+	value := caches[matrix.Point[int]{
+		X:     p.X,
+		Y:     p.Y,
+		Value: p.Value,
+	}]
 	if p.X == h.m.Rows()-1 && p.Y == h.m.Cols()-1 {
 		return
 	}
-	vecs := h.m.GetNeighbor(p.X, p.Y)
+	vecs := h.neighbor(p.X, p.Y)
 	for _, v := range vecs {
-		if limit, ok := dl[getDirection(p, matrix.Point[int]{X: v.X, Y: v.Y})]; ok && limit == 0 {
-			fmt.Printf("skip at %d,%d\n", v.X, v.Y)
+		fmt.Printf("(%d,%d)[%d] -> (%d,%d)[%d]\n", p.X, p.Y, value, v.X, v.Y, v.Value)
+		newdl := make(directionLimit)
+		if limit, ok := dl[getDirection(p.Point, matrix.Point[int]{X: v.X, Y: v.Y})]; ok && limit == 0 {
+			newdl[getDirection(p.Point, matrix.Point[int]{X: v.X, Y: v.Y})] = defaultLimit + 1
 			continue
 		}
-		old, ok := caches[matrix.Point[int]{X: v.X, Y: v.Y}]
-		if !ok {
-			old := h.m.Get(v.X, v.Y)
-			caches[matrix.Point[int]{X: v.X, Y: v.Y}] = value + old
-		} else {
-			if value+h.m.Get(v.X, v.Y) > old {
-				continue
-			} else {
-				caches[matrix.Point[int]{X: v.X, Y: v.Y}] = value + h.m.Get(v.X, v.Y)
-			}
-		}
 		for d := range dl {
-			if d == getDirection(p, matrix.Point[int]{X: v.X, Y: v.Y}) {
-				dl[direction(d)] -= 1
+			if d == getDirection(p.Point, matrix.Point[int]{X: v.X, Y: v.Y}) {
+				newdl[direction(d)] = dl[direction(d)] - 1
 			} else {
-				dl[direction(d)] = defaultLimit
+				newdl[direction(d)] = defaultLimit + 1
 			}
 		}
-		// fmt.Printf("go to %d,%d,dl: %v\n", v.X, v.Y, dl)
-		h.fastPath(matrix.Point[int]{X: v.X, Y: v.Y}, value+h.m.Get(v.X, v.Y), dl)
+		old, ok := caches[matrix.Point[int]{
+			X:     v.X,
+			Y:     v.Y,
+			Value: v.Value,
+		}]
+		if !ok {
+			// fmt.Printf("new: at (%d,%d),dl: %v,value: %d\n", v.X, v.Y, dl, value)
+			caches[matrix.Point[int]{
+				X:     v.X,
+				Y:     v.Y,
+				Value: v.Value,
+			}] = value + v.Value
+		} else {
+			if value+v.Value <= old {
+				// fmt.Printf("set: at (%d,%d),dl: %v,value: %d\n", v.X, v.Y, dl, value)
+				caches[matrix.Point[int]{
+					X:     v.X,
+					Y:     v.Y,
+					Value: v.Value,
+				}] = value + v.Value
+			} else {
+				continue
+			}
+		}
+		points = append(points, vec{
+			Point: v,
+			d:     newdl,
+		})
 	}
+	h.fastPath(points[1:])
 }
 
 const (
@@ -121,15 +168,36 @@ func p1() {
 	hl := &heatLossMap{
 		m: m,
 	}
-	caches[matrix.Point[int]{X: 0, Y: 0}] = m.Get(0, 0)
-	hl.fastPath(matrix.Point[int]{X: 0, Y: 0}, m.Get(0, 0), map[direction]int{
-		up:    defaultLimit,
-		down:  defaultLimit,
-		left:  defaultLimit,
-		right: defaultLimit,
-	})
-	fmt.Printf("%+v\n", caches)
-	fmt.Fprintf(os.Stdout, "p1: %d\n", caches[matrix.Point[int]{X: row - 1, Y: col - 1}])
+	caches[matrix.Point[int]{
+		X:     0,
+		Y:     0,
+		Value: m.Get(0, 0),
+	}] = m.Get(0, 0)
+	v := vec{
+		Point: matrix.Point[int]{
+			X:     0,
+			Y:     0,
+			Value: m.Get(0, 0),
+		},
+		d: map[direction]int{
+			up:    defaultLimit,
+			down:  defaultLimit,
+			left:  defaultLimit,
+			right: defaultLimit,
+		},
+	}
+	hl.fastPath([]vec{v})
+
+	hl.Print()
+
+	fmt.Fprintf(os.Stdout, "p1: %d\n", caches[matrix.Point[int]{
+		X: m.Rows() - 1,
+		Y: m.Cols() - 1,
+		Value: m.Get(
+			m.Rows()-1,
+			m.Cols()-1,
+		),
+	}])
 }
 
 func main() {
