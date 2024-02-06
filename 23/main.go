491c978106437b118dcce822267adeabd8a03e5f
scnace <scbizu@gmail.com>
2024-02-07 02:55:51 +0800

    day23: walk v2

--- 23/main.go
+++ 23/main.go
@@ -4,16 +4,18 @@ import (
 	"container/list"
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/magejiCoder/magejiAoc/grid"
 	"github.com/magejiCoder/magejiAoc/input"
 	"github.com/magejiCoder/magejiAoc/matrix"
 	"github.com/magejiCoder/magejiAoc/queue"
+	"github.com/magejiCoder/magejiAoc/set"
 )
 
 const (
-	line = 141
-	col  = 141
+	line = 23
+	col  = 23
 )
 
 type trails struct {
@@ -28,106 +30,87 @@ type state struct {
 	from    grid.Vec
 }
 
-func (t *trails) walkV2() {
-	q := queue.Queue[state]{
+type stateV2 struct {
+	start  grid.Vec
+	traces *set.Set[grid.Vec]
+}
+
+func direction(from, to grid.Vec) grid.Vec {
+	return grid.Vec{
+		X: to.X - from.X,
+		Y: to.Y - from.Y,
+	}
+}
+
+func (t *trails) walkV2() []int {
+	q := queue.Queue[stateV2]{
 		List: list.New(),
 	}
-	q.Push(state{
-		current: t.start,
-		from:    t.start,
+	q.Push(stateV2{
+		start:  t.start,
+		traces: set.New[grid.Vec](),
 	})
-	t.visited[t.start] = 0
+	var poss []int
 	for {
 		if q.Len() == 0 {
 			break
 		}
 		pp := q.Pop()
-		// fmt.Printf("point: %v\n", pp)
-		start := pp.current
-		from := pp.from
+		start := pp.start
+		trace := pp.traces
+		if start == t.end {
+			fmt.Printf("trace: %d\n", trace.Size())
+			poss = append(poss, trace.Size())
+			continue
+		}
+		if p, ok := t.visited[start]; ok && p > trace.Size() {
+			fmt.Printf("p: %d, trace: %d\n", p, trace.Size())
+			continue
+		} else {
+			if ok {
+				fmt.Printf("p: %d, trace: %d\n", p, trace.Size())
+			}
+			t.visited[start] = trace.Size()
+		}
 		for _, point := range t.m.GetNeighbor(start.X, start.Y) {
+			// 不允许走回头路
+			if trace.Has(grid.Vec(point)) {
+				continue
+			}
 			v := t.m.Get(point.X, point.Y)
 			if v == '#' {
 				continue
 			}
-			// 不走回头路
-			if point.X == from.X && point.Y == from.Y {
-				continue
-			}
-			if p, ok := t.visited[grid.Vec(point)]; ok {
-				if t.visited[start] <= p-1 {
-					continue
-				}
-			}
 			// fmt.Printf("next point: %v\n", point)
 			switch v {
 			// 滑行不会导致撞墙
-			case '>':
+			case '>', '<', '^', 'v':
+				// 在行进方向继续滑行,不需要考虑上下滑梯
 				next := grid.Vec{
-					X: point.X,
-					Y: point.Y + 1,
+					X: point.X + direction(start, grid.Vec(point)).X,
+					Y: point.Y + direction(start, grid.Vec(point)).Y,
 				}
-				// 不能上滑梯
-				if next.X == start.X && next.Y == start.Y {
+				if trace.Has(next) {
 					continue
 				}
-				t.visited[grid.Vec(point)] = t.visited[start] + 1
-				q.Push(state{
-					current: next,
-					from:    start,
+				newTrace := trace.Copy()
+				newTrace.Add(start)
+				newTrace.Add(grid.Vec(point))
+				q.Push(stateV2{
+					start:  next,
+					traces: newTrace,
 				})
-				t.visited[next] = t.visited[start] + 2
-			case '<':
-				next := grid.Vec{
-					X: point.X,
-					Y: point.Y - 1,
-				}
-				if next.X == start.X && next.Y == start.Y {
-					continue
-				}
-				t.visited[grid.Vec(point)] = t.visited[start] + 1
-				q.Push(state{
-					current: next,
-					from:    start,
-				})
-				t.visited[next] = t.visited[start] + 2
-			case '^':
-				t.visited[grid.Vec(point)] = t.visited[start] + 1
-				next := grid.Vec{
-					X: point.X - 1,
-					Y: point.Y,
-				}
-				if next.X == start.X && next.Y == start.Y {
-					continue
-				}
-				q.Push(state{
-					current: next,
-					from:    start,
-				})
-				t.visited[next] = t.visited[start] + 2
-			case 'v':
-				t.visited[grid.Vec(point)] = t.visited[start] + 1
-				next := grid.Vec{
-					X: point.X + 1,
-					Y: point.Y,
-				}
-				if next.X == start.X && next.Y == start.Y {
-					continue
-				}
-				q.Push(state{
-					current: next,
-					from:    start,
-				})
-				t.visited[next] = t.visited[start] + 2
 			default:
-				q.Push(state{
-					current: grid.Vec(point),
-					from:    start,
+				nt := trace.Copy()
+				nt.Add(start)
+				q.Push(stateV2{
+					start:  grid.Vec(point),
+					traces: nt,
 				})
-				t.visited[grid.Vec(point)] = t.visited[start] + 1
 			}
 		}
 	}
+	return poss
 }
 
 func (t *trails) walk() {
@@ -232,7 +215,7 @@ func (t *trails) walk() {
 	}
 }
 
-func main() {
+func p1() {
 	txt := input.NewTXTFile("input.txt")
 	m := matrix.NewMatrix[byte](line, col)
 	start := grid.Vec{}
@@ -255,4 +238,37 @@ func main() {
 	// fmt.Printf("visited: %v\n", ts.visited)
 	distance := ts.visited[end]
 	fmt.Printf("p1: %d\n", distance)
+}
+
+func p2() {
+	txt := input.NewTXTFile("input.txt")
+	m := matrix.NewMatrix[byte](line, col)
+	start := grid.Vec{}
+	end := grid.Vec{}
+	txt.ReadByLineEx(context.Background(), func(i int, l string) error {
+		for j, v := range l {
+			if i == 0 && v == '.' {
+				start = grid.Vec{X: i, Y: j}
+			}
+			if i == line-1 && v == '.' {
+				end = grid.Vec{X: i, Y: j}
+			}
+			m.Add(i, j, byte(v))
+		}
+		return nil
+	})
+	ts := &trails{start: start, m: m, end: end, visited: make(map[grid.Vec]int)}
+	// ts.m.PrintEx("%c")
+	poss := ts.walkV2()
+	sort.Slice(poss, func(i, j int) bool {
+		return poss[i] < poss[j]
+	})
+	fmt.Printf("p2: %d\n", poss[len(poss)-1])
+	// fmt.Printf("visited: %v\n", ts.visited)
+	// fmt.Printf("p2: %d\n", ts.visited[end])
+}
+
+func main() {
+	// p1()
+	p2()
 }
